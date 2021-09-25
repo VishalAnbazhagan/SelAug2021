1b2d52bc217cd580557605d597a72a1f967fc520
VishalAnbazhagan <88607915+VishalAnbazhagan@users.noreply.github.com>
2021-09-25 12:12:45 +0530

    Add files via upload

---
+++ testcase/BaseClass.java
@@ -0,0 +1,69 @@
+package testcase;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.remote.session.FirefoxFilter;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BaseClass {
+	
+	public RemoteWebDriver driver;
+	public String fileName;
+	
+	
+	@DataProvider
+	public String[][] sendData() throws IOException {
+		return ReadExcel.readData(fileName);
+	}
+	
+	@Parameters({"username","password","url","browser"}) //name should exactly matching;order can be anything
+	@BeforeMethod
+	public void preCondition(String uName, String pWord, String url,String browser) { //order should be correct; arg name can be anything
+		if(browser.equalsIgnoreCase("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+		}else if(browser.equalsIgnoreCase("edge")) {
+			WebDriverManager.edgedriver().setup();
+			driver = new EdgeDriver();
+		} else if(browser.equalsIgnoreCase("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		
+		driver.manage().window().maximize();
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys(uName);
+		driver.findElement(By.id("password")).sendKeys(pWord);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+
+	}
+	
+	@AfterMethod
+	public void postCondition() {
+		driver.close();
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ testcase/CreateLead.java
@@ -0,0 +1,32 @@
+package testcase;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class CreateLead extends BaseClass {
+	
+	@BeforeTest
+	public void setFileName() {
+		fileName = "CreateLead";
+
+	}
+	
+
+	@Test(dataProvider = "sendData")
+	public void runCreateLead(String firstName,String lastName,String company, String phNo) {
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys(company);
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys(firstName);
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys(lastName);
+		driver.findElement(By.id("createLeadForm_primaryPhoneNumber")).sendKeys(phNo);
+		driver.findElement(By.name("submitButton")).click();
+
+	}
+		
+	
+}
